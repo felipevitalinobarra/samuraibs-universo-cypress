1c6de9c96fcd041fdf2e9bb2e6d4123b20e6bfbd
Felipe Barra <felipevitalino96@gmail.com>
2024-09-24 16:52:41 -0300

    fix: corrigido caminho do arquivo de configuração do banco e ajustes no uso de variáveis de ambiente

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,9 @@
 const { defineConfig } = require("cypress");
-const { Pool } = require('pg')
+const pool = require('./db');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      const pool = new Pool({
-        host: 'ep-morning-rice-a5b06iwm.us-east-2.aws.neon.tech',
-        user: 'SamuraiBS-Dev_owner',
-        password: 'cXM2fGYF6Spx',
-        database: 'SamuraiBS-Dev',
-        port: 5432,
-        ssl: true
-      })
-
       on('task', {
         removeUser(email) {
           return new Promise(function (resolve) {
