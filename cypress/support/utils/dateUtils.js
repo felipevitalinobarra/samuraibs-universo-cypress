805113e0d398e75a03d0112a407214489c544137
Felipe Barra <felipevitalino96@gmail.com>
2024-10-04 16:30:21 -0300

    Comentado as validações de fim de semana no arquivo "dateUtils.js"

--- cypress/support/utils/dateUtils.js
+++ cypress/support/utils/dateUtils.js
@@ -6,13 +6,13 @@ function getNextBusinessDay() {
     nextBusinessDay.setDate(today.getDate() + 1);
 
     // Se cair em fim de semana, ajusta para o próximo dia útil
-    if (nextBusinessDay.getDay() === 6) {
+    // if (nextBusinessDay.getDay() === 6) {
         // Se for sábado, pula para segunda-feira
-        nextBusinessDay.setDate(nextBusinessDay.getDate() + 2);
-    } else if (nextBusinessDay.getDay() === 0) {
+        // nextBusinessDay.setDate(nextBusinessDay.getDate() + 2);
+    // } else if (nextBusinessDay.getDay() === 0) {
         // Se for domingo, pula para segunda-feira
-        nextBusinessDay.setDate(nextBusinessDay.getDate() + 1);
-    }
+        // nextBusinessDay.setDate(nextBusinessDay.getDate() + 1);
+    // }
 
     return nextBusinessDay.getDate();
 }
